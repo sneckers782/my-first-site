6ca74cb058968fcc0f80f6f70e3cb6330686816d
sneckers782 <alialwawan25@gmail.com>
2025-04-13 06:38:29 +0300

    Create index.lua

---
+++ .github/workflows/index.lua
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+  <meta charset="UTF-8">
+  <title>موقعي الأول</title>
+</head>
+<body>
+  <h1>مرحباً بك!</h1>
+  <p>هذا موقع HTML بسيط مرفوع على GitHub Pages</p>
+</body>
+</html>
